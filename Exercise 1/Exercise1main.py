ed94060614db9af4f3aabd5ff49af17a992a3243
lfcd2 <lfcd2@cam.ac.uk>
2023-11-29 14:14:05 +0000

    Buckyball and PEP 257 compliance

--- Exercise 1/Exercise1main.py
+++ Exercise 1/Exercise1main.py
@@ -8,7 +8,12 @@ BETA = -1.
 
 # Create Matrix
 def create_huckel_matrix(n=4, mode='linear'):
-
+    """
+    Creates a huckel matrix based off the length and mode inputted
+    :param int n: length of polyene if linear or cyclic
+    :param str mode: type of matrix to be generated
+    :return array matrix: returns the matrix
+    """
     matrix = np.zeros((n, n))
 
     for i in range(n-1):
@@ -58,15 +63,23 @@ def create_huckel_matrix(n=4, mode='linear'):
                               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
                               [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0]])
 
+    elif mode == 'buckyball':
+        matrix = -np.loadtxt(open("bucky.csv", "rb"), delimiter=",")
+
     return matrix
 
 
 def get_evals(matrix):
+    """Return the eigenvalues of the matrix"""
     eigenvals = np.linalg.eigvals(matrix)
     return eigenvals
 
 
 def get_input():
+    """
+    Gets the desired length for linear and cyclic polyenes
+    :return int n: integer length
+    """
     n = input('n: ')
     try:
         n = int(n)
@@ -80,6 +93,10 @@ def get_input():
 
 
 def get_type():
+    """
+    Gets the type of huckel matrix to be calculated
+    :return str input_mode: the mode of matrix to be calculated
+    """
     input_mode = input('Mode (cyclic, linear, tetrahedron, cube, dodecahedron or buckyball): ')
     if input_mode not in ['cyclic', 'linear', 'cube', 'tetrahedron', 'dodecahedron', 'buckyball']:
         input_mode = get_type()
@@ -87,6 +104,11 @@ def get_type():
 
 
 def count_degeneracies(eigenvals):
+    """
+    Counts and prints the degeneracies of the eigenvalues
+    :param list eigenvals: list of all eigenvalues (floats)
+    :return:
+    """
     degeneracies = []
 
     for i, a in enumerate(eigenvals):
@@ -98,13 +120,15 @@ def count_degeneracies(eigenvals):
 
     degeneracies = sorted(degeneracies)
     for energy, degeneracy in degeneracies:
-        print(f'Energy: α{-1*energy:+}β, Degeneracy: {degeneracy}')
+        print(f'Energy: α{-1*energy.real:+}β, Degeneracy: {degeneracy}')
+    print('')
 
 
 if __name__ == "__main__":
-    molecule = get_type()
-    size = get_input() if molecule in ['cyclic', 'linear'] else 1
-    print('')
-    master_matrix = create_huckel_matrix(size, mode=molecule)
-    evals = list(get_evals(master_matrix))
-    count_degeneracies(evals)
+    while True:
+        molecule = get_type()
+        size = get_input() if molecule in ['cyclic', 'linear'] else 1
+        print('')
+        master_matrix = create_huckel_matrix(size, mode=molecule)
+        evals = list(get_evals(master_matrix))
+        count_degeneracies(evals)
