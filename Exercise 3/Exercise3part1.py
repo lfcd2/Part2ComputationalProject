a1f50964111206e58b566b0f4952fdf585fc1fb2
lfcd2 <lfcd2@cam.ac.uk>
2024-01-19 18:07:35 +0000

    pep8

--- Exercise 3/Exercise3part1.py
+++ Exercise 3/Exercise3part1.py
@@ -1,25 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-urea = np.arange(0, 8, 0.01)
 
-kf15 = 26000*np.exp(-1.68*urea)
-ku15 = 0.06*np.exp(0.95*urea)
-kf16 = 730*np.exp(-1.72*urea)
-ku16 = 7.5e-4*np.exp(1.2*urea)
+def run_ex3p1() -> None:
+    urea = np.arange(0, 8, 0.01)
 
-concentration_I = 1/((ku15/kf15)+(kf16/ku16)+1)
-concentration_D = concentration_I * ku15/kf15
-concentration_N = concentration_I * kf16/ku16
+    kf15 = 26000*np.exp(-1.68*urea)
+    ku15 = 0.06*np.exp(0.95*urea)
+    kf16 = 730*np.exp(-1.72*urea)
+    ku16 = 7.5e-4*np.exp(1.2*urea)
 
-plt.plot(urea, concentration_D, color='blue', label='D')
-plt.plot(urea, concentration_I, color='green', label='I')
-plt.plot(urea, concentration_N, color='red', label='N')
-plt.legend(loc='center left')
-plt.xlabel('Fraction of species')
-plt.ylabel('[Urea]/M')
-plt.title('Equilibrium fractions of protein states against urea concentration')
-plt.xlim(0, 8)
-plt.ylim(-0.01, 1.01)
-plt.savefig('Part1Output.png', dpi=300)
-plt.show()
+    concentration_I = 1/((ku15/kf15)+(kf16/ku16)+1)
+    concentration_D = concentration_I * ku15/kf15
+    concentration_N = concentration_I * kf16/ku16
+
+    plt.plot(urea, concentration_D, color='blue', label='D')
+    plt.plot(urea, concentration_I, color='green', label='I')
+    plt.plot(urea, concentration_N, color='red', label='N')
+    plt.legend(loc='center left')
+    plt.xlabel('Fraction of species')
+    plt.ylabel('[Urea]/M')
+    plt.title('Equilibrium fractions of protein states against urea concentration')
+    plt.xlim(0, 8)
+    plt.ylim(-0.01, 1.01)
+    plt.savefig('Part1Output.png', dpi=300)
+    plt.show()
+
+
+if __name__ == "__main__":
+    run_ex3p1()
